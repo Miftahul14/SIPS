57c825bb5dd1afb6d63e2dc93b2780fc9c81e1f8
miftahul14 <mifkingdom@gmail.com>
2025-09-18 11:57:47 +0700

    settings baru

--- poin/settings.py
+++ poin/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS", default="*")]
+ALLOWED_HOSTS = ["sips-production.up.railway.app", "127.0.0.1", "localhost"]
 
 
 # Application definition
